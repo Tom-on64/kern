af1d9d6da7945059f03fe5c4a5df0b35596cd580
Tom-on64 <kubecekt@gmail.com>
2024-09-09 08:18:15 +0200

    Added config

--- src/stage2.asm
+++ src/stage2.asm
@@ -369,9 +369,11 @@ DATA_SEG equ GDT.dataDescriptor - GDT.start
 
 ;; VBE Stuff
 ;; XXX IF YOU WANT TO SELECT ANY VALUE IN THE SETUP, SET THESE TO ZERO XXX
-width: dw 0
-height: dw 0
-bpp: db 0
+%ifdef DEF_RES
+width: dw 1920
+%endif
+height: dw 1080
+bpp: db 32
 
 offset: dw 0
 _segment: dw 0 ; segment is a keyword
