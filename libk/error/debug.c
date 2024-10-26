4079049242204304306c9da80abc2bd6c1297de2
Tom-on64 <kubecekt@gmail.com>
2024-10-26 14:45:07 +0200

    Fixed debugf(); OS now works, but immediatly triggers a Page Fault after printing a 'k'

--- libk/error/debug.c
+++ libk/error/debug.c
@@ -1,12 +1,18 @@
 #include <error/debug.h>
 #include <ports/serial.h>
+#include <stdio.h>
 #include <stdarg.h>
+
+static void _dbgPutc(char c, void* arg) {
+    (void)arg;
+    serialWrite(PORT_COM1, c);
+    if (c == '\n') { serialWrite(PORT_COM1, '\r'); }
+}
 
 int debugf(const char* fmt, ...) {
     va_list va;
     va_start(va, fmt);
-    serialPrint(PORT_COM1, fmt);    // TODO: use a printf
-    int res = 0;
+    int res = vpprintf(_dbgPutc, NULL, fmt, va);
     va_end(va);
     return res;
 }
